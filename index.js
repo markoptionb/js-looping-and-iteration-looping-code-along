ef007fadeb55d9f30a5849ce1ee639d203066266
markoptionb <optionbsupport@gmail.com>
2021-03-15 13:39:33 +1100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const arr1 = ['Lisa', 'Kaitlin', 'Jan']
+
+function writeCards(arr1){
+    let arr2 = [];
+    for (let i = 0; i < arr1.length; i++) {
+        arr2.push (`Thank you, ${arr1[i]}, for the wonderful surprise gift!`);
+    }
+    return arr2
+};
+
+let f = 10;
+function countDown(f){
+    while (f >= 0)
+        console.log(f--); 
+};
